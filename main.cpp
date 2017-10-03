7cd2cc96052e5b1832c74f0b0d8292908698cb75
Matthias Behr <mbehr@mcbehr.de>
2017-10-03 14:07:21 +0200

    restart on sighup internally

--- main.cpp
+++ main.cpp
@@ -14,12 +14,20 @@
 
 #include "engine.h"
 
+bool gRestart = false;
+
 // taken from: https://gist.github.com/azadkuh/a2ac6869661ebd3f8588
 // no license. assuming free or MIT.
 void catchUnixSignals(std::initializer_list<int> quitSignals) {
     auto handler = [](int sig) -> void {
         // blocking and not aysnc-signal-safe func are valid
-        printf("\nquit the application by signal(%d).\n", sig);
+        if (sig == SIGHUP) {
+            printf("\nrestarting the application by signal(%d).\n", sig);
+            gRestart = true;
+        } else {
+            printf("\nquit the application by signal(%d).\n", sig);
+            gRestart = false;
+        }
         QCoreApplication::quit();
     };
 
@@ -39,10 +47,13 @@ void catchUnixSignals(std::initializer_list<int> quitSignals) {
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP});
-
-    Engine engine;
-
-    return a.exec();
+    int ret=0;
+    do {
+        gRestart = false;
+        QCoreApplication a(argc, argv);
+        catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP});
+        Engine engine;
+        ret = a.exec();
+    } while (gRestart);
+    return ret;
 }
